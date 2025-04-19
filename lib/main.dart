71ae80f56f8833e6db8b059606915de73aa43a48
ibnul.imtiaz <imtiazeemel42@gmail.com>
2025-04-19 12:34:06 +0600

    Flutter MVVM Architecture With Rest APIs Using Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_state/provider/auth_login_provider.dart';
-import 'package:provider_state/provider/container_provider.dart';
-import 'package:provider_state/provider/count_provider.dart';
-import 'package:provider_state/provider/dynamic_theme_provider.dart';
-import 'package:provider_state/provider/favourite_provider.dart';
-import 'package:provider_state/provider/password_visibility_provider.dart';
-import 'package:provider_state/view/another_screen.dart';
-import 'package:provider_state/view/dynamic_theme_screen.dart';
-import 'package:provider_state/view/favourite_screen.dart';
-import 'package:provider_state/view/home_view.dart';
-import 'package:provider_state/view/login_view.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -22,45 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => CountProvider()),
-        ChangeNotifierProvider(create: (context) => ContainerProvider()),
-        ChangeNotifierProvider(create: (context) => FavouriteProvider()),
-        ChangeNotifierProvider(create: (context) => DynamicThemeChangerProvider()),
-        ChangeNotifierProvider(create: (context) => PasswordVisibilityProvider()),
-        ChangeNotifierProvider(create: (context) => AuthProvider()),
-
-      ],
-      child: Builder(builder: (BuildContext context){
-        final themeChanger = Provider.of<DynamicThemeChangerProvider>(context);
-        return MaterialApp(
-          title: 'Flutter Demo',
-          themeMode: themeChanger.themeMode,
-          theme: ThemeData(
-            useMaterial3: false,
-            brightness: Brightness.light,
-            primarySwatch: Colors.red,
-            appBarTheme: AppBarTheme(
-            backgroundColor: Colors.purple,
-            actionsIconTheme: IconThemeData(color: Colors.red),
-            ),
-          ),
-          darkTheme: ThemeData(
-              useMaterial3: false,
-            brightness: Brightness.dark,
-            primarySwatch: Colors.purple,
-            primaryColor: Colors.purple,
-            appBarTheme: AppBarTheme(
-              backgroundColor: Colors.teal,
-              actionsIconTheme: IconThemeData(color: Colors.purple),
-            ),
-          ),
-          home:  LoginView(),
-        );
-      }),
-      
-    );
+    return Scaffold();
   }
 }
 
