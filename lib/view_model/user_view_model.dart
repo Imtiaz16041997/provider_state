25941e6f91c04ce0954219d65b657ad5ac9ea8f6
ibnul.imtiaz <imtiazeemel42@gmail.com>
2025-04-21 22:50:44 +0600

    login updated with response and sharedpref

---
+++ lib/view_model/user_view_model.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/user_model.dart';
+
+class UserViewModel with ChangeNotifier {
+
+  Future<bool> saveUser(UserModel userResponse) async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    //String userJson = jsonEncode(userResponse.toJson());
+     sp.setString('user', userResponse.token.toString());
+     notifyListeners();
+    return true;
+  }
+
+  Future<UserModel?> getUser() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    final String? token = sp.getString('token');
+
+    return UserModel(
+      token: token.toString()
+    );
+    // String? userJson = sp.getString('user');
+    // if (userJson != null) {
+    //   Map<String, dynamic> userMap = jsonDecode(userJson);
+    //   return UserResponse.fromJson(userMap);
+    // } else {
+    //   return null;
+    // }
+  }
+
+  // Future<String?> getToken() async {
+  //   final user = await getUser();
+  //   return user?.result?.accessToken;
+  // }
+
+  Future<bool> removeUser() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.clear();
+    return true;
+  }
+}
