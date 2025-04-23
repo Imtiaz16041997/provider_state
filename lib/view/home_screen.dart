361ff9744be539b628cc287824b595dd51c774bd
ibnul.imtiaz <imtiazeemel42@gmail.com>
2025-04-23 23:24:05 +0600

    pagination

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -15,83 +15,115 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  HomeViewModel homeViewModel = HomeViewModel();
-  
+  final HomeViewModel homeViewModel = HomeViewModel();
+  final ScrollController _scrollController = ScrollController();
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    homeViewModel.fetchMoviesListApi();
+    homeViewModel.fetchMovies(); // fetch all and paginate manually
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent - 200) {
+        homeViewModel.loadMore();
+      }
+    });
   }
-  
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build');
     final userPreferences = Provider.of<UserPreferencesViewModel>(context);
-    
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
         automaticallyImplyLeading: false,
         actions: [
           InkWell(
-            onTap: (){
-              userPreferences.removeUser().then((value){
+            onTap: () {
+              userPreferences.removeUser().then((value) {
                 Navigator.pushNamed(context, RoutesName.loginScreen);
               });
             },
-            child: Text('Logout'),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text('Logout', style: TextStyle(color: Colors.black)),
+            ),
           )
         ],
       ),
       backgroundColor: Colors.black,
-      body: ChangeNotifierProvider<HomeViewModel>(
-        create: (BuildContext context) => homeViewModel,
+      body: ChangeNotifierProvider<HomeViewModel>.value(
+        value: homeViewModel,
         child: Consumer<HomeViewModel>(
-          builder: (context,value,child){
-          switch(value.movieList.status){
-            case Status.LOADING:
-              return Center(child: CircularProgressIndicator());
-            case Status.COMPLETED:
-              return ListView.builder(
-                itemCount: value.movieList.data?.movies?.length,
-                  itemBuilder: (context,index){
-                    return Card(
-                      color: Colors.green,
-                      child: ListTile(
-                        leading: Image.network(
-                          value.movieList!.data!.movies![index].posterurl.toString(),
-                          errorBuilder: (context,error,stack){
-                            return Icon(Icons.error,color: Colors.red,);
-                          },
-                          height: 40,
-                          width: 40,
-                          fit: BoxFit.cover,
+          builder: (context, value, child) {
+            switch (value.movieList.status) {
+              case Status.LOADING:
+                return Center(child: CircularProgressIndicator());
+              case Status.ERROR:
+                return Center(
+                    child: Text(value.movieList.message.toString(),
+                        style: TextStyle(color: Colors.white)));
+              case Status.COMPLETED:
+                return ListView.builder(
+                  controller: _scrollController,
+                  itemCount: value.movies.length + 1, // +1 for loader
+                  itemBuilder: (context, index) {
+                    if (index < value.movies.length) {
+                      final movie = value.movies[index];
+                      return Card(
+                        color: Colors.green,
+                        child: ListTile(
+                          leading: Image.network(
+                            movie.posterurl ?? '',
+                            errorBuilder: (context, error, stackTrace) =>
+                                Icon(Icons.error, color: Colors.red),
+                            height: 40,
+                            width: 40,
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(movie.title!),
+                          subtitle: Text(movie.storyline!),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(Utils.setAverageRating(movie.ratings!)
+                                  .toString()),
+                              Icon(Icons.star, color: Colors.yellowAccent),
+                            ],
+                          ),
                         ),
-                        title: Text(value.movieList!.data!.movies![index].title.toString()),
-                        subtitle: Text(value.movieList!.data!.movies![index].storyline.toString()),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(Utils.setAverageRating(value.movieList!.data!.movies![index].ratings!).toString()),
-                            Icon(Icons.star,color: Colors.yellowAccent,)
-                          ],
+                      );
+                    } else {
+                      return value.isFetchingMore
+                          ? Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: CircularProgressIndicator(),
                         ),
-                      
-                      ),
-                    );
-                  }
-              );
-            case Status.ERROR:
-            return Center(child: Text(value.movieList.message.toString(),style: TextStyle(color: Colors.white),));
-
-            default:
-              return Text('Unknown state');
-          }
-        }),
+                      )
+                          : SizedBox.shrink();
+                    }
+                  },
+                );
+              default:
+                return Text('Unknown state', style: TextStyle(color: Colors.white));
+            }
+          },
+        ),
       ),
     );
   }
 }
+
 
 
       /*SafeArea(
